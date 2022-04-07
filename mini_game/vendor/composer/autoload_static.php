cfb45ed09a89a41eedad436caaf7c23645e01f23
Excellence KAWEJ <19ka201@esisalama.org>
2022-04-07 17:53:15 +0200

    modifying the composer.json file

--- mini_game/vendor/composer/autoload_static.php
+++ mini_game/vendor/composer/autoload_static.php
@@ -6,13 +6,37 @@ namespace Composer\Autoload;
 
 class ComposerStaticInit328b00b155edfc965b9243988ff478ca
 {
+    public static $prefixLengthsPsr4 = array (
+        'P' => 
+        array (
+            'Player\\' => 7,
+        ),
+        'M' => 
+        array (
+            'MiniGame\\' => 9,
+        ),
+    );
+
+    public static $prefixDirsPsr4 = array (
+        'Player\\' => 
+        array (
+            0 => __DIR__ . '/../..' . '/src',
+        ),
+        'MiniGame\\' => 
+        array (
+            0 => __DIR__ . '/../..' . '/src',
+        ),
+    );
+
     public static $classMap = array (
-        'Composer\\InstalledVersions' => __DIR__ . '/..',
+        'Composer\\InstalledVersions' => __DIR__ . '/..' . '/composer/InstalledVersions.php',
     );
 
     public static function getInitializer(ClassLoader $loader)
     {
         return \Closure::bind(function () use ($loader) {
+            $loader->prefixLengthsPsr4 = ComposerStaticInit328b00b155edfc965b9243988ff478ca::$prefixLengthsPsr4;
+            $loader->prefixDirsPsr4 = ComposerStaticInit328b00b155edfc965b9243988ff478ca::$prefixDirsPsr4;
             $loader->classMap = ComposerStaticInit328b00b155edfc965b9243988ff478ca::$classMap;
 
         }, null, ClassLoader::class);
